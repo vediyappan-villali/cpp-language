34527af4ec51084d9f424e7d4d386477d8eada38
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-03 14:40:19 +0530

    Add hello world program
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+// CPP program to print hello world
+// To compile the program: g++ <file-name> or c++ <file-name>
+// To run the program: ./a.out
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
