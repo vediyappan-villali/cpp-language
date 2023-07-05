bf2cb53a4a4c27e5ab176b3bfe394a007144a10f
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:29:45 +0530

    Add program to understand boolean data type
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 016_bool.cpp
@@ -0,0 +1,18 @@
+// C++ program to understand boolean datatype
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool flag = true;
+   
+    cout << "flag = " << flag << endl;
+
+    flag = false;
+
+    cout << "flag = " << flag << endl;
+
+    return 0;
+}
