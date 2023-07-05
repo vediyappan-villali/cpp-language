283c230d97e2ae2c9511c55afa51d4f65ce5c33a
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:37:53 +0530

    Add program to understand limitation of reference variable
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 023_reference-variable_limitation1.cpp
@@ -0,0 +1,20 @@
+// C++ program to understand reference variable aka call by reference
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int &y;
+    y = num;
+
+    cout << "num = " << num << endl;
+    cout << "y   = " << y   << endl;
+
+    cout << "&num  = " << &num  << endl;
+    cout << "&y    = " << &y    << endl;
+
+    return 0;
+}
