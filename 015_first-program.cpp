4832e1cd74b07fba9e9fb24641201de3b30d59f3
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:28:49 +0530

    Add program to scan a number and print it
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 015_first-program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter value of num: ";
+    cin  >> num;
+
+    cout << "Number = " << num << endl;
+
+    return 0;
+}
