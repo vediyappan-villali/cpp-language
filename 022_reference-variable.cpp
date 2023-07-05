4f5cf7d2fbb5b3bb86253c355ea373536c72ff20
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:36:25 +0530

    Add program to understand reference variable
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 022_reference-variable.cpp
@@ -0,0 +1,19 @@
+// C++ program to understand reference variable aka call by reference
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int &y  = num;
+
+    cout << "num = " << num << endl;
+    cout << "y   = " << y   << endl;
+
+    cout << "&num  = " << &num  << endl;
+    cout << "&y    = " << &y    << endl;
+
+    return 0;
+}
