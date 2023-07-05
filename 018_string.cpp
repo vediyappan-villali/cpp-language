bb7b520ac02f933b281c1616b014686bdb114af7
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:31:41 +0530

    Add program to understand string data type
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 018_string.cpp
@@ -0,0 +1,14 @@
+// C++ program to understand string datatype
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str = "Hello! Welcome!";
+
+    cout << "str = " << str << endl;
+
+    return 0;
+}
