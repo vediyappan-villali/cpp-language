6422594bef3532c9be3144abd7b6b4faa6afe3ec
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-06 00:13:06 +0530

    Add program to understand class
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 045_class.cpp
@@ -0,0 +1,33 @@
+// C++ program to understand the class
+
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    private:
+        int num;
+    public:
+        void set()
+        {
+            num = 10;
+        }
+        void get()
+        {
+            cout << "num = " << num << endl;
+        }
+};
+
+int main()
+{
+    A a1, a2;
+
+    a1.set();
+
+    a1.get();
+
+    a2.get();
+
+    return 0;
+}
