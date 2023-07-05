37fd54b170186f554437d086f938fa53c0e59410
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:27:12 +0530

    Add program to understand istream
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 013_istream-cin.cpp
@@ -0,0 +1,17 @@
+// C++ program to understand standard input stream i.e., cin
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char name[64];
+
+    cout << "Enter name: ";
+    cin  >> name;
+
+    cout << "name = " << name << endl;
+
+    return 0;
+}
