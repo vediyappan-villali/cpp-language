f1ee764f5ed4d267d94f6a3fafe808141b0bf81b
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:26:22 +0530

    Add program to understand ostream
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 012_ostream-cout.cpp
@@ -0,0 +1,14 @@
+// C++ program to understand ostream i.e., cout
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[] = "Hello C++";
+
+    cout << "String = " << str << endl;
+
+    return 0;
+}
