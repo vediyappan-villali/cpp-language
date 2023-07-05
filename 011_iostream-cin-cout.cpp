23d574de86cfd1aaf7561e6e88b53842bbcbba1a
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:25:34 +0530

    Add program to understand iostream
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 011_iostream-cin-cout.cpp
@@ -0,0 +1,16 @@
+// C++ program to understand iostream i.e., cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter value of num: ";
+    cin  >> num;
+    cout << "num = " << num << endl;
+
+    return 0;
+}
