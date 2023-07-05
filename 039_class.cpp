4606d142f5dcff4402ba713c07e8565b78483200
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-06 00:08:41 +0530

    Add program to understand class
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 039_class.cpp
@@ -0,0 +1,21 @@
+// C++ program to understand the class
+
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    int num;
+};
+
+int main()
+{
+    A a1;
+
+    a1.num = 10;
+
+    cout << "a1.num = " << a1.num << endl;
+
+    return 0;
+}
