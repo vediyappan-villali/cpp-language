c67d65755878bfac9129ffc1c19e16d6ef09865c
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:20:35 +0530

    Add program to understand data abstraction
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 008_data-abstraction.cpp
@@ -0,0 +1,24 @@
+// C++ program to understand data abstraction
+
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    int  num;
+    void func()
+    {
+    }
+};
+
+int main()
+{
+    A a1, a2, a3;
+
+    a1.func();
+    a2.func();
+    a3.func();
+
+    return 0;
+}
