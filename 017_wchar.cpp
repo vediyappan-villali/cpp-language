80b221483448373a7f5320acea1f1ccab4681a0e
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:30:57 +0530

    Add program to understand wchar_t
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 017_wchar.cpp
@@ -0,0 +1,14 @@
+// C++ program to understand wchar_t
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    wchar_t wch = 'a';
+
+    cout << "wch = " << wch << endl;
+
+    return 0;
+}
