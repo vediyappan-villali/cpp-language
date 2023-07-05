c2ccb483b3e957c20b6470bb2b5fa652176bdbcc
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:01:03 +0530

    Add program to understand function overloading
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 002_function-overloading.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void add(int num1, int num2)
+{
+    cout << "Sum = " << num1 + num2 << endl;
+}
+
+void add(char num1, int num2)
+{
+    cout << "Sum = " << num1 + num2 << endl;
+}
+void add(int num1, int num2, int num3)
+{
+    cout << "Sum = " << num1 + num2 + num3 << endl;
+}
+
+int main()
+{
+    add(10, 20);
+    
+    add('a', 20);
+
+    add(10, 20, 30);
+
+    return 0;
+}
