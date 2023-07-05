2396282e61c4d2d3e416f179642a3c5a3e695557
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-05 23:32:52 +0530

    Add program to understand string concatenation
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 019_string-concat.cpp
@@ -0,0 +1,16 @@
+// C++ program to understand string concatenation
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1 = "Vediyappan";
+    string str2 = " Villali";
+    string str3 = str1 + str2;
+
+    cout << "str3 = " << str3 << endl;
+
+    return 0;
+}
