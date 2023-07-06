ca6c68e65fdc26e0afa1cecab24cf9c097eae918
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-06 14:20:29 +0530

    Add program to understand destructor
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ 074_destructor.cpp
@@ -0,0 +1,34 @@
+// C++ program to understand the destructor
+
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    private:
+        int num;
+    public:
+        A()
+        {
+            cout << "In constructor ..." << endl;
+        }
+        ~A()
+        {
+            cout << "In destructor ..." << endl;
+        }
+};
+
+int main()
+{
+    A a1;
+
+    {
+        A a2;
+        cout << "In block ..." << endl;
+    }
+
+    cout << "In main ..." << endl;
+
+    return 0;
+}
